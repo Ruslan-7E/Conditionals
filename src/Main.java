cbe68ebf93ffbfe32939aa2c44c3752d48b847a4
Ruslan Mukhametshin <ruslanow597@gmail.com>
2022-08-05 17:32:57 +0300

    Finish hw3, 5 tasks.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,75 @@
 public class Main {
     public static void main(String[] args) {
+        // task 1
+        int clientOS = 1;
 
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
 
+        // task 2
+        int clientDeviceYear = 2017;
 
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
 
+        // task 3
+        int year = 2021;
 
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
 
+        // task 4
+        int deliveryDistance = 95;
+        int deliveryDays = 1;
+
+        if (deliveryDistance > 20) {
+            deliveryDays++;
+        }
+
+        if (deliveryDistance > 60) {
+            deliveryDays++;
+        }
+
+        System.out.println("Потребуется дней: " + deliveryDays);
+
+        // task 5
+        int monthNumber = 12;
+
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Autumn");
+                break;
+            default:
+                System.out.println("Incorrect month " + monthNumber);
+        }
 
 
 //        int age = 19;
