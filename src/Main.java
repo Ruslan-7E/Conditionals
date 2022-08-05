6705d7bb89e26523976cbf99f22502cd072be9c6
Ruslan Mukhametshin <ruslanow597@gmail.com>
2022-08-05 15:21:19 +0300

    Start hw3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+
+
+
+
+
+//        int age = 19;
+//        boolean isSkyClear = true;
+//        boolean canNotGoToSchool = age < 7 || age > 18;
+//
+//        if (age >= 18 && age <= 21) {
+//            System.out.println("Ты можешь водить, но не можешь бухать.");
+//        }
+//
+//        if (canNotGoToSchool) {
+//            System.out.println("You probably don't go to school.");
+//        }
+//
+//        if (isSkyClear) {
+//            System.out.println("The sky is clear.");
+//        }
     }
 }
+
